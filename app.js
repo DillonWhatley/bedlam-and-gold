f224d151dba506d47709e1f5c9b77ee5c262ab5d
newoc1 <owencapo1@gmail.com>
2016-02-27 14:08:18 -0500

    serving index.html from / and opening up public and node_modules directories

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.redirect('index.html');
 });
 
 app.listen(3000, function() {

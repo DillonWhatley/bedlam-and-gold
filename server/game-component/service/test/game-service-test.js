7512e65d2fe6b430e1e8cb2714f6a58b8d14fd89
newoc1 <owencapo1@gmail.com>
2016-03-15 22:06:02 -0400

    bag-20: mocking required modules using proxyquire.

--- server/game-component/service/test/game-service-test.js
+++ server/game-component/service/test/game-service-test.js
@@ -1,14 +1,27 @@
+var proxyquire = require('proxyquire');
 var assert = require('assert');
-var gameService = require('../game-service');
-var databaseConfig = require('../../../config/databaseConfig');
-var mongoose = require('mongoose');
-mongoose.connect(databaseConfig.getMongoURI());
+var gameDAOStub = {};
+var monsterServiceStub = {};
+var gameService = proxyquire('../game-service', {
+  '../data-access/game-dao': gameDAOStub,
+  './monster-service': monsterServiceStub
+});
 
 describe('GameService', function() {
   describe('#create()', function() {
     this.timeout(10000);
     it('should return a new game instance', function(done) {
-      gameService.create(function(err, game) {
+      gameDAOStub.save = function(game, callback) {
+        return callback(game);
+      };
+      gameDAOStub.Game = function() {
+        this.monsters = [];
+      };
+      monsterServiceStub.generateMonster = function(callback) {
+        var monster = {};
+        return callback(monster);
+      };
+      gameService.create(function(game) {
         assert.equal(1, game.monsters.length, 'No monsters were added');
         assert.notEqual(game, null, 'Game was not initialized.');
         done();
